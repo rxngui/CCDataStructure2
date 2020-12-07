6c3b0c605802e7e8aacb387442097082f3fba2f9
rxngui <mateus2015men@outlook.com>
2020-12-07 13:43:23 -0300

    Implementação 2.0 pilha

--- Revisão EDI/Stack.c
+++ Revisão EDI/Stack.c
@@ -2,6 +2,14 @@
 #include<ctype.h>
 #include<stdlib.h>
 
+struct stack{
+    int info;
+    struct stack *prox;
+
+}; typedef struct stack Stack;
+
+
+
 int main(){
     printf("Compiled Sucessfully");
     return 1;
