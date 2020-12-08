1cd723a2cd558b42a3631dc0bf10f4061a13ea5b
rxngui <mateus2015men@outlook.com>
2020-12-08 16:33:33 -0300

    implementation of priority queue

---
+++ Revisão EDI/priorityQueue.h
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+struct priorityqueue{
+    int info, priority;
+    struct priorityqueue *next;
+};
+typedef struct priorityqueue PriorQueue;
+
+void init(PriorQueue **QP){
+    *QP = NULL;
+}
+void Enqueue(PriorQueue **QP, int x, int p){
+    PriorQueue *new = (PriorQueue*)malloc(sizeof(PriorQueue));
+    new->next = NULL;
+    new->
+}
